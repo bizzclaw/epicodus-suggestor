697a8187dd4215ac092fdf9cc92dec7cc257946a
bizzclaw <bizzclaw1@gmail.com>
2017-09-15 14:45:17 -0700

    Add display output function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
 	var name = "";
 
-	var fadetime = 1300;
+	var fadetime = 700;
+
+	var displayOutput = function(title, description) {
+		fadeSwap(".questionaire", "#output");
+		$("#output-title").text(title);
+		$("#output-description").text(description);
+	};
 
 	var fadeSwap = function(class1, class2) {
 		$(class1).fadeOut(fadetime);
@@ -14,8 +20,24 @@ $(document).ready(function() {
 
 	$("#form-name").submit(function(event) {
 		event.preventDefault();
+		name = $("#input-name").val();
 		fadeSwap("#questions-name", "#questions-initial")
-		name = $("input-name").val();
 	});
+
+	$("#form-initial").submit(function(event) {
+		event.preventDefault();
+		
+		var selected = $("input:radio[name=radio-initial]:checked").val();
+
+		if (selected === "backend") {
+
+		} else {
+			displayOutput("CSS/Design track", "The best option for those interested in designing and styling web pages or web sites, especially if you have a background in graphic design.")
+		}
+	});
+
+	$("#reset").click(function(){
+		fadeSwap("#output", "#questions-initial")
+	})
 
 });
