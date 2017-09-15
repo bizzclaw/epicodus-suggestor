4febaf5ca56842c2be9fa0c1ea63c1f162bcf81a
bizzclaw <bizzclaw1@gmail.com>
2017-09-15 14:47:43 -0700

    simplify fadeSwap function to only require a single argument

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,15 @@ $(document).ready(function() {
 	var fadetime = 700;
 
 	var displayOutput = function(title, description) {
-		fadeSwap(".questionaire", "#output");
+		fadeSwap("#output");
 		$("#output-title").text(title);
 		$("#output-description").text(description);
 	};
 
-	var fadeSwap = function(class1, class2) {
-		$(class1).fadeOut(fadetime);
+	var fadeSwap = function(newclass) {
+		$(".questionaire").fadeOut(fadetime);
 		setTimeout(function(){
-			$(class2).fadeIn(fadetime);
+			$(newclass).fadeIn(fadetime);
 		}, fadetime)
 	};
 
@@ -21,23 +21,23 @@ $(document).ready(function() {
 	$("#form-name").submit(function(event) {
 		event.preventDefault();
 		name = $("#input-name").val();
-		fadeSwap("#questions-name", "#questions-initial")
+		fadeSwap("#questions-initial")
 	});
 
 	$("#form-initial").submit(function(event) {
 		event.preventDefault();
-		
+
 		var selected = $("input:radio[name=radio-initial]:checked").val();
 
 		if (selected === "backend") {
-
+			fadeSwap("#questions-weborother")
 		} else {
 			displayOutput("CSS/Design track", "The best option for those interested in designing and styling web pages or web sites, especially if you have a background in graphic design.")
 		}
 	});
 
 	$("#reset").click(function(){
-		fadeSwap("#output", "#questions-initial")
+		fadeSwap("#questions-initial")
 	})
 
 });
