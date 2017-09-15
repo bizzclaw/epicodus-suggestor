c9025ad6a3be9ab4419c151ce5957e3052289c67
bizzclaw <bizzclaw1@gmail.com>
2017-09-15 13:54:39 -0700

    Add fadeSwap function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
+	var name = "";
+
+	var fadetime = 1300;
+
+	var fadeSwap = function(class1, class2) {
+		$(class1).fadeOut(fadetime);
+		setTimeout(function(){
+			$(class2).fadeIn(fadetime);
+		}, fadetime)
+	};
+
+	$("#questions-name").fadeIn(fadetime);
+
+	$("#form-name").submit(function(event) {
+		event.preventDefault();
+		fadeSwap("#questions-name", "#questions-initial")
+		name = $("input-name").val();
+	});
 
 });
